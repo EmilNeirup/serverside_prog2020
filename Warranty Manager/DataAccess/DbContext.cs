0e7a121f2ad6d2bcb8ef13999d3efa8ca9843b79
Mikkel Norre Nielsen <mikkel@norre.cc>
2020-04-20 12:49:41 +0200

    Rename

---
+++ Warranty Manager/DataAccess/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Warranty Manager/DataAccess/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class Product
+    {
+    }
+}
